e8f43a3b2ac30e877a520e4e88e762fdad36c60f
stanley-E-5025 <stanlyumana@Stanlys-MacBook-Air.local>
2022-08-12 17:12:35 -0600

    fix db conections

--- src/providers/databaseprovider.ts
+++ src/providers/databaseprovider.ts
@@ -8,11 +8,11 @@ export const DatabaseProvider = [
     useFactory: async () =>
       createConnection({
         type: 'postgres',
-        host: 'fanny.db.elephantsql.com',
+        host: 'ec2-34-201-95-176.compute-1.amazonaws.com',
         port: 5432,
-        username: 'mlktskna',
-        password: 'bYOnzcBrLz2mAlW09A3rHvolgG7R2Ht7',
-        database: 'mlktskna',
+        username: 'sqecpswsrlzigg',
+        password: 'fccf800607ef7350884edcb16ee35a36382d69e0c0fdab5ab54d5826b779213e',
+        database: 'sqecpswsrlzigg',
         entities: [users, videos],
         synchronize: true,
       }),
